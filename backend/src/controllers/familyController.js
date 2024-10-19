ee63ba60051c5ae7efdeea656232c03a775f7ad5
RohitChand12 <rohitchand490@gmail.com>
2024-10-19 15:54:49 +0530

    get responses of family api

--- backend/src/controllers/familyController.js
+++ backend/src/controllers/familyController.js
@@ -3,19 +3,27 @@ const Response = require("../models/response");
 
 exports.getFamily = async (req, res) => {
   try {
-    const { house_no, father_first_name, father_last_name, survey_id } = req.query;
+    const { house_no, father_first_name, father_last_name, survey_id } =
+      req.query;
 
     const filterOptions = { survey_id };
-    
-    if (house_no) filterOptions.house_no = { $regex: new RegExp(`^${house_no}$`, 'i') };
-    if (father_first_name) filterOptions.father_first_name = { $regex: new RegExp(`^${father_first_name}$`, 'i') };
-    if (father_last_name) filterOptions.father_last_name = { $regex: new RegExp(`^${father_last_name}$`, 'i') };
+
+    if (house_no)
+      filterOptions.house_no = { $regex: new RegExp(`^${house_no}$`, "i") };
+    if (father_first_name)
+      filterOptions.father_first_name = {
+        $regex: new RegExp(`^${father_first_name}$`, "i"),
+      };
+    if (father_last_name)
+      filterOptions.father_last_name = {
+        $regex: new RegExp(`^${father_last_name}$`, "i"),
+      };
 
     const families = await Family.find(filterOptions);
 
     res.status(200).json({
       success: true,
-      data: families
+      data: families,
     });
   } catch (error) {
     console.error(error);
@@ -23,25 +31,48 @@ exports.getFamily = async (req, res) => {
   }
 };
 
-
-exports.updateFamily = async (req,res)=>{
-    try{
-        const {family_id,house_no, father_first_name, father_last_name} = req.body
-        const updatedFamily = {}
-        if(house_no) updatedFamily.house_no = house_no
-        if(father_first_name) updatedFamily.father_first_name = father_first_name
-        if(father_last_name) updatedFamily.father_last_name = father_last_name
-        const family = await Family.findByIdAndUpdate(family_id, updatedFamily, {new:true})
-
-        if (Object.keys(updatedFamily).length > 0) {
-            await Response.updateMany(
-              { family_id }, 
-              { $set: updatedFamily } 
-            );
-        }
-
-        res.status(200).json({success:true, data:family})
-    }catch(error){
-        res.status(500).json({success:false, message:"Internal server error"})
+exports.getFamilyResponse = async (req, res) => {
+  try {
+    const { family_id } = req.query;
+    if(!family_id){
+      res.status(400).json({ success: false, message:"family_id is required"})
     }
-}
+
+    const family = await Family.findById(family_id);
+    const responses = await Response.find({ family_id });
+
+    res.status(200).json({
+      success: true,
+      family,
+      responses,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success:false,
+      message:"Internal server error"
+    })
+  }
+};
+
+exports.updateFamily = async (req, res) => {
+  try {
+    const { family_id, house_no, father_first_name, father_last_name } =
+      req.body;
+    const updatedFamily = {};
+    if (house_no) updatedFamily.house_no = house_no;
+    if (father_first_name) updatedFamily.father_first_name = father_first_name;
+    if (father_last_name) updatedFamily.father_last_name = father_last_name;
+    const family = await Family.findByIdAndUpdate(family_id, updatedFamily, {
+      new: true,
+    });
+
+    if (Object.keys(updatedFamily).length > 0) {
+      await Response.updateMany({ family_id }, { $set: updatedFamily });
+    }
+
+    res.status(200).json({ success: true, data: family });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+};
--- backend/src/routes/familyRoutes.js
+++ backend/src/routes/familyRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const familyController = require("../controllers/familyController");
 
 router.get("/get",familyController.getFamily)
+router.get("/get-responses",familyController.getFamilyResponse)
 router.post("/update",familyController.updateFamily)
 
 module.exports = router;
