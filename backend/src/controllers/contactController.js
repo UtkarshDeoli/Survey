1dcbba081715788dffa530e353f85a8a0d38e8db
RohitChand12 <rohitchand490@gmail.com>
2024-11-28 16:21:05 +0530

    contact callback

--- backend/src/controllers/contactController.js
+++ backend/src/controllers/contactController.js
@@ -75,4 +75,10 @@ exports.initiateCall = async (req, res) => {
     });
   }
 };
-
+exports.contactCallback = async(req,res)=>{
+  console.log("body from callback ---->",req.body)
+  console.log("body from query ---->",req.query)
+  return res.status(200).json({
+    message:"callback hit successfully"
+  })
+}
--- backend/src/routes/contactRoutes.js
+++ backend/src/routes/contactRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { sendSMS, initiateCall } = require('../controllers/contactController');
+const { sendSMS, initiateCall, contactCallback } = require('../controllers/contactController');
 const router = express.Router();
 router.post('/send-sms',sendSMS)
 router.post('/call',initiateCall)
+router.get('/callback',contactCallback)
 
 module.exports = router
