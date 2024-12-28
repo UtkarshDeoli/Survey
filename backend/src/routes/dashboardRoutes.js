2a91d1bd4ff8b8c7254ad3d80847fdd98ca4333b
RawatDevanshu <rawatdevanshu22@gmail.com>
2024-12-28 17:23:43 +0530

    karyakarta response status count

--- backend/src/routes/dashboardRoutes.js
+++ backend/src/routes/dashboardRoutes.js
@@ -3,11 +3,15 @@ const {
   getDashboard,
   getResponsesStatusCount,
   getResponsesByStatus,
+  getKaryakartaAssignedResponsesStatus,
 } = require("../controllers/dashboardController");
 const router = express.Router();
 
 router.get("/", getDashboard);
 router.get("/responses-status", getResponsesStatusCount);
 router.get("/responses", getResponsesByStatus);
+router.get(
+  "/karyakarta-responses-status",
+  getKaryakartaAssignedResponsesStatus,
+);
 module.exports = router;
-
