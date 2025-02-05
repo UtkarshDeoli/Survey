3f6c34d7f2dbcc3e594ae65fa0be82856b3a167c
Rohit Chand <rohitchand490@gmail.com>
2025-02-05 10:02:09 +0530

    Payload too large error fix

--- backend/app.js
+++ backend/app.js
@@ -26,8 +26,9 @@ if (process.env.NODE_ENV === "development") {
 
 // app.use(fileUpload());
 app.use(cors({ origin: "*", exposedHeaders: ["Content-Disposition"] }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 app.use("/api/survey/", surveyRoutes);
 app.use("/api/auth/", authRoutes);
